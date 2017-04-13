fa813ab3541e109dbe5506ccf3332bc303478fcc
Olivia Bettaglio <olivia.bettaglio@gmail.com>
2017-04-12 18:36:35 -0700

    Data reshaping practice.

--- digits.py
+++ digits.py
@@ -4,7 +4,23 @@ digits = datasets.load_digits()
 
 classifier = svm.SVC(gamma=0.001, C=100.)
 
+
+# train with all but last item in dataset
+# test with last item in dataset
 classifier.fit(digits.data[:-1], digits.target[:-1])
 prediction = classifier.predict(digits.data[-1:])
 
 print prediction
+
+
+# reshape data
+# transform image into vector (length=64)
+import matplotlib.pyplot as plt
+print digits.images.shape
+print str(digits.images.shape[0]) + " images of"
+print str(digits.images.shape[1]) + " height by"
+print str(digits.images.shape[2]) + " width"
+
+plt.imshow(digits.images[-1], cmap=plt.cm.gray_r)
+
+data = digits.images.reshape((digits.images.shape[0], -1))
--- iris.py
+++ iris.py
@@ -6,8 +6,15 @@ clf = svm.SVC()
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
 
+# understanding dataset shape
+print X.shape
+print str(X.shape[0]) + " observations of irises"
+print str(X.shape[1]) + " features: sepal and petal length/width"
+
+# train with all data
 clf.fit(X, y)
 
+# test with last item in dataset
 s = pickle.dumps(clf)
 clf2 = pickle.loads(s)
 prediction = clf2.predict(X[:1])
